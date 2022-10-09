15f9e3708e864cc7f25063126e05a08e99e8d423
Vagner Nerves <vagnernervessantos@gmail.com>
2022-10-09 19:38:28 -0300

    feat: Created NewTask and implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Header } from './components/Header'
+import { NewTask } from './components/NewTask'
+
+import style from './App.module.css'
 
 export function App() {
   return (
     <div className="App">
       <Header />
 
-      <main></main>
+      <main className={style.main}>
+        <NewTask />
+      </main>
     </div>
   )
 }
